6c16592ad29e9bf4c3e4c28ab354f823117de7ee
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-26 17:27:37 +0700

    create nilai ke table nilai

---
+++ tambah_data.php
@@ -0,0 +1,13 @@
+<?php 
+  include "koneksi.php";
+
+  if ($_POST['tombol_simpan']) {
+    $nim = $_POST['nim'];
+    $nilai = $_POST['nilai'];
+    $kode_mk = $_POST['kode_mk'];
+    $thn_akademik = $_POST['thn_akademik'];
+
+    mysqli_query($conn, "INSERT INTO nilai (nim, kode_mk, nilai, thn_akademik) VALUES($nim, '$kode_mk', '$nilai', '$thn_akademik')");
+  }
+  $q_tambah = mysqli_query($conn, "select nim, nm_mhs from m_mahasiswa");
+?>
